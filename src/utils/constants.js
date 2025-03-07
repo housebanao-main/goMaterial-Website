eb9accae7d653f9ec77d43a975f5b5f0219680d4
Priyanshu <codepriyanshu@gmail.com>
2025-03-07 14:14:40 +0530

    Update API URL for local development in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 export const IMG_PLACEHOLDER= "https://placehold.jp/700x700.png"
 
-// export const API_URL= "https://go-material.vercel.app/app";
-export const API_URL= "https://6bm5lmq7-4000.inc1.devtunnels.ms/app";
+export const API_URL= "https://go-material.vercel.app/app";
+// export const API_URL= "https://6bm5lmq7-4000.inc1.devtunnels.ms/app";
 // export const API_URL= "http://localhost:4000/app";
